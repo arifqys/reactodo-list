4f648646617be36f56b7fa31c439f5a6d29b432f
arifqys <arifqys@gmail.com>
2018-07-30 16:05:22 +0700

    Add functionality to add list

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      currentTodo: ""
+    };
+  }
+  onInputChange = e => {
+    this.setState({currentTodo: e.target.value});
+  }
+  onClick = () => {
+    let todosCopy = this.state.todos.slice();
+    todosCopy.push(this.state.currentTodo);
+    this.setState({todos: todosCopy, currentTodo:""});
+  }
   render() {
+    let bulletedTodos = this.state.todos.map((e, i) => {
+      return (
+        <li key={i}>{e}</li>
+      );
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input placeholder="Next todo" value={this.state.currentTodo} onChange={this.onInputChange}/>
+        <button onClick={this.onClick}>Add</button>
+        <br/>
+        { this.state.todos.length === 0 ? "There is no todo" : <ul>{bulletedTodos}</ul> }
       </div>
     );
   }
